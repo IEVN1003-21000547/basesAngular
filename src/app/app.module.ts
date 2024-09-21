9fe6e82300f39c4f003dbd42a21aad14b025938a
Victor Emiliano <vickb11k3@gmail.com>
2024-09-20 19:49:48 -0600

    viernes 20 ejemplo1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product/product-list/product-list.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ProductoFilterPipe } from './product/producto-filter.pipe';
 import { OperasBasComponent } from './formularios/operas-bas/operas-bas.component';
+import { Ejemplo1Component } from './formularios/ejemplo1/ejemplo1.component';
+import { CinepolisAngularComponent } from './formularios/cinepolis-angular/cinepolis-angular.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ProductoFilterPipe,
-    OperasBasComponent
+    OperasBasComponent,
+    Ejemplo1Component,
+    CinepolisAngularComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/formularios/cinepolis-angular/cinepolis-angular.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cinepolis-angular',
+  templateUrl: './cinepolis-angular.component.html',
+  styleUrl: './cinepolis-angular.component.css'
+})
+export class CinepolisAngularComponent {
+
+}
